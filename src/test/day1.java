a3edfa5376fee8dc151e7e7c74bbc6617ea07666
Beatriz <bpsanchezp@grupodelaware.com>
2020-05-08 16:03:18 -0500

    this is commiy from perú

--- src/test/day1.java
+++ src/test/day1.java
@@ -8,6 +8,8 @@ public class day1 {
 	@Test
 	public void demo() {
 		System.out.println("hola mundo");
+		System.out.println("soy beatriz");
+		System.out.println("vamo con todo");
 	}
 	@Test
 	public void secondTest() {
